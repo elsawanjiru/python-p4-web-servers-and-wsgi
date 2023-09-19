04ede236ce6d76c061ab7f1ee3dac0a39e75b3fd
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-09-19 11:28:04 +0300

    Completed code challenge

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    return Response('Hello, World!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 5555, application)
